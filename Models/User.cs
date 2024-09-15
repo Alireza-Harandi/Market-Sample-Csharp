e49e8048cc6261ccf54b68712578b8ebd93d6123
Alireza <aliharadi10@gmail.com>
2024-09-16 00:21:50 +0330

    create Data class create Admin, User class(subclasses of the Data class)

--- Models/User.cs
+++ Models/User.cs
@@ -1,6 +1,34 @@
 ﻿namespace DotNetHW2;
 
-public class User
+public abstract class Data
 {
+    public String name { get; set; }
+    public string username { get; set; }
+    public string password { get; set; }
+    public static List<Data> dataList = new List<Data>();
+
+    public static Data mainData { get; set; }
     
+    public Data(String name, string username, string password)
+    {
+        this.name = name;
+        this.username = username;
+        this.password = password;
+    }
+}
+
+public class Admin : Data
+{
+    public Admin(string name, string username, string passsword) : base(name, username, passsword)
+    {
+    }
+}
+
+public class User : Data
+{
+    public double credit { get; set; }
+    public User(string name, string username, string passsword) : base(name, username, passsword)
+    {
+        this.credit = 0;
+    }
 }
