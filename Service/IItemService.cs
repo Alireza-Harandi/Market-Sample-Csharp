018f6b72715bc4f52cbbb518a0c23fa5dbc57e6e
Alireza <aliharadi10@gmail.com>
2024-09-16 00:23:05 +0330

    create interface for controller the method

--- Service/IItemService.cs
+++ Service/IItemService.cs
@@ -2,5 +2,15 @@
 
 public interface IItemService
 {
-    
+    public bool Signup(string name, string username, string password);
+    public bool Login(string username, string password);
+    public bool EditProfile(string name);
+    public bool AddItem(string name);//for Admin
+    public bool RemoveItem(string name);//for Admin
+    public string BuyItem(string name);
+    public bool IncreaseCredit(double amount);
+    public bool SignupAdmin(string name, string username, string password);
+    public void Logout();
+    public string GetInfoAdmin();
+    public string GetInfoUser();
 }
