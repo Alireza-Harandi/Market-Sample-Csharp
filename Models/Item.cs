7f261aea07db00ad29b9ecdd42bfbf5f2a15b923
Alireza <aliharadi10@gmail.com>
2024-09-16 00:20:02 +0330

    create Item class create Phone,Computer class (subclasses of the Item class)

--- Models/Item.cs
+++ Models/Item.cs
@@ -1,5 +1,33 @@
 ﻿namespace DotNetHW2;
 
-public class Item
+public abstract class Item
 {
+    public string name { get; set; }
+    public double price { get; set; }
+
+    public Item(string name, double price)
+    {
+        this.name = name;
+        this.price = price;
+    }
+}
+
+public class Phone : Item
+{
+    public static double price = 100;
+    public static int count = 0;
+    public Phone() : base("Phone", price)
+    {
+        count++;
+    }
+}
+
+public class Computer : Item
+{
+    public static double price = 200;
+    public static int count=0;
+    public Computer() : base("Computer", price)
+    {
+        count++;
+    }
 }
